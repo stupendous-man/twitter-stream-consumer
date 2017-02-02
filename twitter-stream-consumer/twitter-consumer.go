7e7113e89cdb115290ce96c8a81a27c947c3e8ed
stupendous-man <idiegoromero@gmail.com>
2017-02-01 22:27:46 -0500

    Modified twitter-consumer.go

--- twitter-stream-consumer/twitter-consumer.go
+++ twitter-stream-consumer/twitter-consumer.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"github.com/dghubble/oauth1"
-	"github.com/dghubble/go-twitter/twitter"
-	"os"
 	"fmt"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"os"
 )
 
 func main() {
+
 	config := oauth1.NewConfig(os.Getenv("TWITTER_CONSUMER_KEY"), os.Getenv("TWITTER_CONSUMER_SECRET"))
 	token := oauth1.NewToken(os.Getenv("TWITTER_ACCESS_TOKEN"), os.Getenv("TWITTER_ACCESS_SECRET"))
 
@@ -15,10 +16,20 @@ func main() {
 
 	client := twitter.NewClient(httpClient)
 
-	tweets, _, _ := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{Count: 10})
+	tweets, _, _ := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{Count: 5})
 
-	fmt.Printf("User's HOME TIMELINE:\n%+v\n", tweets)
+	for _, tweet := range tweets {
 
-	//TODO: Retrieve tweet Entities
-	//TODO: Insert tweet Entities in mongodb
+		fmt.Println(tweet.Text)
+
+		for _, url := range tweet.Entities.Urls {
+			fmt.Println("Display URL:", url.DisplayURL)
+			fmt.Println("Expanded URL:", url.ExpandedURL)
+			fmt.Println("URL:", url.URL)
+		}
+
+		fmt.Println()
+	}
+
+	//TODO: Insert tweet Entity URLs in mongodb
 }
