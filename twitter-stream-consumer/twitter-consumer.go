c31c02d13ad251a1f2bb4687763d1d410d8ae71d
stupendous-man <idiegoromero@gmail.com>
2017-02-03 22:16:40 -0500

    Modified twitter-consumer.go

--- twitter-stream-consumer/twitter-consumer.go
+++ twitter-stream-consumer/twitter-consumer.go
@@ -5,6 +5,9 @@ import (
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 	"os"
+	"gopkg.in/mgo.v2"
+	"log"
+	"time"
 )
 
 func main() {
@@ -34,6 +37,43 @@ func main() {
 	}
 
 	//TODO: Insert tweet Entity URLs in mongodb
+}
+
+func mongoInsert(simpleTweet SimpleTweet) {
+	//Establish session with Mongo server
+	session, err := mgo.Dial(os.Getenv("MONGO_PORT_27017_TCP_ADDR") + ":" + os.Getenv("MONGO_PORT_27017_TCP_PORT"))
+
+	if err != nil {
+		//Retry a few times before going into panic
+		log.Print(err)
+		for retries := 1; retries <= 5; retries++ {
+			log.Printf("Retrying Mongo connection. Attempt %d...", retries)
+			session, err = mgo.Dial("localhost")
+
+			if err != nil {
+				time.Sleep(100 * time.Millisecond)
+			} else {
+				break
+			}
+		}
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	//Deferred function to close session
+	defer session.Close()
+
+	//Establish connection with database/collection
+	c := session.DB("goctupusdb").C("events")
+
+	//Insert document in database/collection
+	err = c.Insert(&simpleTweet)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 type SimpleTweet struct {
